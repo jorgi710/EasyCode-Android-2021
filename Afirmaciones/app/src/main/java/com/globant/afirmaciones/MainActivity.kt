04ce8891eb8a300d305e94902cd72e36395ef172
JORGI <jorgi710@hotmail.com>
2021-11-24 15:21:30 -0500

    v1.0_Afirmaciones

--- Afirmaciones/app/src/main/java/com/globant/afirmaciones/MainActivity.kt
+++ Afirmaciones/app/src/main/java/com/globant/afirmaciones/MainActivity.kt
@@ -1,16 +1,11 @@
-package com.globant.cumple
+package com.globant.afirmaciones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val sayButton: Button = findViewById(R.id.btnNumber)
-
-
     }
 }
