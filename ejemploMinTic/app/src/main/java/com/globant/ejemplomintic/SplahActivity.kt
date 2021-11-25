584e5136c49af7e30022c53e55f48ba9f67ae718
JORGI <jorgi710@hotmail.com>
2021-11-25 08:16:02 -0500

    v1.1 Creation del Splash:
         Diseño sencillo, cargamos una imagen la ubicamos en el centro, especificamos el layout, description, un progress

---
+++ ejemploMinTic/app/src/main/java/com/globant/ejemplomintic/SplahActivity.kt
@@ -0,0 +1,11 @@
+package com.globant.ejemplomintic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplahActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splah)
+    }
+}
